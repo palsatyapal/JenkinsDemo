bcccaf90540ed3777960250e9ef38a23f98b6bed
palsatyapal <100669809+palsatyapal@users.noreply.github.com>
2022-03-03 17:02:09 +0530

    Update JenkinEx.java

--- JenkinEx.java
+++ JenkinEx.java
@@ -2,6 +2,8 @@ class JenkinEx{
 public static void main(String [] args){
 System.out.println("hello Jenkins I'm coming from Github ");
 System.out.println("hello jenkins im here ");
+  System.out.println("hello jenk ");
+  System.out.println("hello jenkins im h ");
 
 }
 }
